7008b9e971bd6990dda4618cc54ed3f95aca4ad4
aloneen <seisenbek2005@mail.ru>
2025-10-13 12:34:11 +0500

    First commit

---
+++ src/main/java/kz/seisen/mid_rpo/MidRpoApplication.java
@@ -0,0 +1,13 @@
+package kz.seisen.mid_rpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidRpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidRpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/seisen/mid_rpo/MidRpoApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.seisen.mid_rpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidRpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
